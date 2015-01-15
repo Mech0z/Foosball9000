dcbbf00964df04018c3dfe618976d02e225cfa09
Mads Skipper <madsskipper@gmail.com>
2015-01-15 21:45:09 +0100

    fix

--- RavenStarterKit/Controllers/LeaderBoardController.cs
+++ RavenStarterKit/Controllers/LeaderBoardController.cs
@@ -26,11 +26,6 @@ namespace MvcPWy.Controllers
                 var commandProcessingResult = CommandProcessingResult.WithNewPosition(lastSequenceNumber.Value);
                 await _leaderboardViewManager.WaitUntilProcessed(commandProcessingResult, new TimeSpan(0, 0, 5));
             }
-            else
-            {
-                var commandProcessingResult = CommandProcessingResult.WithNewPosition(11);
-                await _leaderboardViewManager.WaitUntilProcessed(commandProcessingResult, new TimeSpan(0, 0, 5));
-            }
             
             var view = _leaderboardViewManager.Load("__global__");
             return View(view);
