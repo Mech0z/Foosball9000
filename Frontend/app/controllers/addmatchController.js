03428ee9409482dbe79d893c9628e5c5480c98b8
Mads Skipper <madsskipper@gmail.com>
2015-04-29 21:33:49 +0200

    add match stuff

--- Frontend/app/controllers/addmatchController.js
+++ Frontend/app/controllers/addmatchController.js
@@ -3,7 +3,14 @@
 app.controller("addmatchController",
     function($scope, match, user) {
 
+        $scope.match = {};
+
         user.getUsers().then(function(payload) {
-            $scope.userList= payload;
+            $scope.userList = payload;
+
         });
+
+        $scope.submit = function() {
+            console.log($scope.match);
+        };
     });
