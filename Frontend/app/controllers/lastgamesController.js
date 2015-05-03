8a5c9636f494220c18046842056e85f457cfa60d
Mads Skipper <madsskipper@gmail.com>
2015-05-03 20:39:19 +0200

    loading on last games

--- Frontend/app/controllers/lastgamesController.js
+++ Frontend/app/controllers/lastgamesController.js
@@ -1,11 +1,20 @@
 ﻿"use strict";
 
 app.controller("lastgamesController",
-    function ($scope, match) {
-                
+    function($scope, match) {
+
+        $scope.errorMessage = "";
+        $scope.loading = true;
+
         $scope.lastgamesPromise = match.getLatest(5);
 
-        $scope.lastgamesPromise.then(function (payload) {
+        $scope.lastgamesPromise.then(function(payload) {
             $scope.lastgames = payload;
+            $scope.loading = false;
+        }, function(error) {
+            $scope.loading = false;
+            $scope.errorMessage = error;
+            $scope.loadingFailed = true;
+            console.log(error);
         });
     });
