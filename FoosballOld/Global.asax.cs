af4f0772ab0c593b759d1af90b5db0e220782f63
Jasper Nygaard <jasper@sovs.net>
2015-05-08 19:21:47 +0200

    Added Logger class and Loggly configuration

--- FoosballOld/Global.asax.cs
+++ FoosballOld/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System.Reflection;
+using System.Web;
 using System.Web.Http;
 using System.Web.Http.Dispatcher;
 using System.Web.Mvc;
@@ -6,6 +7,7 @@ using System.Web.Optimization;
 using System.Web.Routing;
 using Castle.MicroKernel.Registration;
 using Castle.Windsor;
+using Common.Logging;
 using FoosballOld.ContainerInstallers;
 using MongoDBRepository;
 
@@ -31,7 +33,10 @@ namespace FoosballOld
             Container = new WindsorContainer();
 
             Container.Install(new MongoInstaller(), new LogicInstaller());
+
+            Container.Register(Classes.FromAssemblyNamed("Common").BasedOn<ILogger>().WithServiceAllInterfaces().LifestylePerWebRequest());
             Container.Register(Classes.FromThisAssembly().BasedOn<ApiController>().LifestylePerWebRequest());
+
 
             GlobalConfiguration.Configuration.Services.Replace(
                 typeof(IHttpControllerActivator), new WindsorCompositionRoot(Container));
@@ -39,7 +44,7 @@ namespace FoosballOld
 
         public override void Dispose()
         {
-            //TODO WHy is contianre null causing a null exception :s
+            //TODO WHy is container null causing a null exception :s
             //Container.Dispose();
             base.Dispose();
         }
