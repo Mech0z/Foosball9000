476f32507a48ce69268bcee831bf26f32eecea83
soeberg84 <msoeberg84@gmail.com>
2015-05-10 21:53:43 +0200

    Automatch and add up to three matches! With terrible UI.

--- Frontend/app/controllers/startmatchController.js
+++ Frontend/app/controllers/startmatchController.js
@@ -2,8 +2,10 @@
 
 app.controller("startmatchController",
     function ($scope, $q, $location, leaderboard, match, user) {
-
-        $scope.match = {};
+        $scope.playerlist = {};
+        $scope.match1 = {};
+        $scope.match2 = {};
+        $scope.match3 = {};
         $scope.validationFailed = false;
         $scope.errorMessage = "";
         $scope.loading = true;
@@ -35,12 +37,73 @@ app.controller("startmatchController",
 
         $scope.playerClick = function (user) {
             $scope.bestMatchup.addPlayer(user);
+        };
 
-            if($scope.bestMatchup.IsMatchReady)
-            {
-                //do stuff
+        $scope.doStuff = function () {
+            
+            var p1 = $scope.bestMatchup.autoMatchup[0];            
+            var p2 = $scope.bestMatchup.autoMatchup[1];            
+            var p3 = $scope.bestMatchup.autoMatchup[2];            
+            var p4 = $scope.bestMatchup.autoMatchup[3];
+            
+            var matchList = [];
+            if (typeof $scope.match1.MatchResult !== 'undefined') {
+                if ($scope.match1.MatchResult.Team1Score > 0 || $scope.match1.MatchResult.Team2Score > 0) {
+                    matchList.push($scope.match1);
+                }
+            }
+
+            if (typeof $scope.match2.MatchResult !== 'undefined') {
+                if ($scope.match2.MatchResult.Team1Score > 0 || $scope.match2.MatchResult.Team2Score > 0) {
+                    matchList.push($scope.match2);
+                }
+            }
+
+            if (typeof $scope.match3.MatchResult !== 'undefined') {
+                if ($scope.match3.MatchResult.Team1Score > 0 || $scope.match3.MatchResult.Team2Score > 0) {
+                    matchList.push($scope.match3);
+                }
+            }
+
+            for (var i = 0; i < matchList.length; i++) {
+                matchList[i].PlayerList = [p1, p2, p3, p4];
+
+                var validationResult = match.validateMatch($scope.match1);
+                if (!validationResult.validated)
+                {
+                    $scope.validationFailed = true;
+                    $scope.errorMessage = validationResult.errorMessage;
+                    return;
+                }
+            }
+            
+            for (var i = 0; i < matchList.length; i++) {
+                $scope.addMatch(matchList[i], i == matchList.length - 1)
             }
         };
+
+        $scope.addMatch = function (m, navigate) {
+            
+            //$scope.Debug = $scope.Debug + " " + m.MatchResult.Team1Score;
+
+            $scope.validationFailed = false;
+            $scope.loading = true;
+            var addMatchPromise = match.addMatch(m);
+
+            addMatchPromise.then(function () {
+                $scope.loading = false;
+                console.log(m);
+                if (navigate) {
+                    $location.path("leaderboard");
+                }
+            }, function () {
+                $scope.loading = false;
+                $scope.errorMessage = "Request failed ";
+                $scope.validationFailed = true;
+            });
+
+            return;
+        }
     });
 
 var CalculateBestMatchup = function () {
@@ -52,6 +115,8 @@ var CalculateBestMatchup = function () {
     this.playerlist = [];
     
     this.autoMatchup = [];
+
+    this.orderedPlayerList = [];
 
     this.addPlayer = function (p) {
 
@@ -90,7 +155,6 @@ var CalculateBestMatchup = function () {
         }
 
         this.Message = "All players selected!!";
-        this.Debug = "All players selected!!";
 
         var p1 = this.playerlist[0];
         var p2 = this.playerlist[1];
@@ -128,10 +192,7 @@ var CalculateBestMatchup = function () {
 
         this.Message = bestMatchup.t1p1.UserName + " and " + bestMatchup.t1p2.UserName
         + " vs " + bestMatchup.t2p1.UserName + " and " + bestMatchup.t2p2.UserName;
-
-        this.Debug = "points team 1: (" + bestMatchup.t1points + ") " + bestMatchup.t1p1.UserName + " and " + bestMatchup.t1p2.UserName
-        + " vs team 2: (" + bestMatchup.t2points + ") " + bestMatchup.t2p1.UserName + " and " + bestMatchup.t2p2.UserName;
-        
+              
         this.autoMatchup = [bestMatchup.t1p1.UserName, bestMatchup.t1p2.UserName, bestMatchup.t2p1.UserName, bestMatchup.t2p2.UserName];
 
         return true;
