9ce0c66382fdb7bb84a71e5ad259509c0a256f2c
Jasper Nygaard <jasper@sovs.net>
2015-05-20 23:27:49 +0200

    Build script adds commit message to release notes.

--- default.ps1
+++ default.ps1
@@ -10,6 +10,8 @@ Task Default -Depends Clean,CompileBackend, PackFrontend, CreateRelease
 
 
 Task Clean {
+
+    Write-Host $releasenotes
     remove-item $here\*.nupkg -force
 }
 
@@ -27,7 +29,7 @@ Task CreateRelease {
     if($OctopusApiKey) {
         Exec {& $Nuget  install octopustools -OutputDirectory tools -Version 2.6.1.52 } "Failed to install octopus tools"
         $Octo = $here+'\tools\OctopusTools.2.6.1.52\octo.exe'
-        Exec { & $Octo create-release --server http://octopus.sovs.net:5602 --apikey $OctopusApiKey --project Foosball9000 --enableservicemessages --version $Version --deployto Staging --package=FoosballOld:$Version --package=FoosballFrontend:$Version } "Failed to create Octopus release"
+        Exec { & $Octo create-release --server http://octopus.sovs.net:5602 --apikey $OctopusApiKey --project Foosball9000 --enableservicemessages --version $Version --releasenotes $releasenotes --deployto Staging --package=FoosballOld:$Version --package=FoosballFrontend:$Version } "Failed to create Octopus release"
     } else {
         Write-Host "Skipping creating release"
     }
