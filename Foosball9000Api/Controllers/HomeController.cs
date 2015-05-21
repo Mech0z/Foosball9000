5d2645ec71cd2b90f2f33eb3ff5c15ba63359943
Jasper Nygaard <jasper@sovs.net>
2015-05-21 23:07:50 +0200

    Renamed FoosballOld to Foosball9000Api. Deleted obsolete mvc code.

---
+++ Foosball9000Api/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Configuration;
+using System.Web.Mvc;
+
+namespace Foosball9000Api.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            var version = ConfigurationManager.AppSettings["Version"];
+            var environment = ConfigurationManager.AppSettings["Environment"];
+            return string.Format("Foosball9000 API v{0} ({1})", version, environment);
+        }
+    }
+}
---
+++ Foosball9000Api/Global.asax.cs
@@ -0,0 +1,55 @@
+﻿using System.Web;
+using System.Web.Http;
+using System.Web.Http.Dispatcher;
+using System.Web.Http.ExceptionHandling;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using Common.Exceptions;
+using Common.Logging;
+using Foosball9000Api.ContainerInstallers;
+
+namespace Foosball9000Api
+{
+    public class WebApiApplication : HttpApplication
+    {
+        public IWindsorContainer Container { get; private set; }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            ContainerInstaller();
+        }
+
+        private void ContainerInstaller()
+        {
+            Container = new WindsorContainer();
+
+            Container.Install(new MongoInstaller(), new LogicInstaller());
+
+            Container.Register(
+                Classes.FromAssemblyNamed("Common")
+                    .BasedOn<ILogger>()
+                    .WithServiceAllInterfaces()
+                    .LifestylePerWebRequest());
+            Container.Register(Classes.FromThisAssembly().BasedOn<ApiController>().LifestylePerWebRequest());
+
+            GlobalConfiguration.Configuration.Services.Replace(
+                typeof (IHttpControllerActivator), new WindsorCompositionRoot(Container));
+
+            GlobalConfiguration.Configuration.Services.Add(typeof (IExceptionLogger),
+                new TraceExceptionLogger(new Logger()));
+        }
+
+        // ReSharper disable once RedundantOverridenMember
+        public override void Dispose()
+        {
+            //TODO WHy is container null causing a null exception :s
+            //Container.Dispose();
+            base.Dispose();
+        }
+    }
+}
